cceaba50dfc3e8aac735c7d841af6b5b4ccd7f40
Cade Quigley <caquigley@alaska.edu>
2026-04-26 11:43:04 -0800

    Did some pep8 formatting.

--- array_figures.py
+++ array_figures.py
@@ -13,7 +13,8 @@ from array_functions import get_geometry
 ############################################################
 
 
-def record_section(st, stations, sta_lats, sta_lons, event, eq_lat, eq_lon, mag, channel, plot_type):
+def record_section(st, stations, sta_lats, sta_lons, event, eq_lat, eq_lon, 
+                   mag, channel, plot_type):
 
     fig, ax = plt.subplots(figsize = (10,8))
 
@@ -27,12 +28,17 @@ def record_section(st, stations, sta_lats, sta_lons, event, eq_lat, eq_lon, mag,
         ypos = dist/1000
         distance.append(ypos)
         time_range = np.max(tr.times()) - np.min(tr.times())
-        ax.plot(tr.times()-time_range/2,ypos+((tr.data/(4*max(tr.data)))), color = 'black', alpha = 0.8)
-        ax.text(1.01, ypos, station, transform=trans, color = 'black', fontweight = 'bold',fontsize = 10, ha="left", va="center")
+        ax.plot(tr.times()-time_range/2,ypos+((tr.data/(4*max(tr.data)))), 
+                color = 'black', alpha = 0.8)
+        ax.text(1.01, ypos, station, transform=trans, color = 'black', 
+                fontweight = 'bold',fontsize = 10, ha="left", va="center")
     
     plt.axvline(x=0, color = 'red', linestyle = '--')
     distance = np.array(distance)
-    ax.text(0.05, max(distance)+0.25, 'Event: '+event+'; M'+str(mag)+'; '+channel, transform = trans, fontsize = 15, fontweight = 'bold', color = 'firebrick')
+    ax.text(0.05, max(distance)+0.25,
+            'Event: '+event+'; M'+str(mag)+'; '+channel,
+            transform = trans, fontsize = 15, fontweight = 'bold',
+            color = 'firebrick')
     ax.set_xlabel('Time since estimated arival time (s)')
     ax.set_ylabel('Distance from earthquake (km)')
     if plot_type =='far':
@@ -68,7 +74,8 @@ def trigger_timing(st, start):
 ############################################################
 
 
-def histogram(values, lower_quantile, upper_quantile, variable_name, save = False, path = None):
+def histogram(values, lower_quantile, upper_quantile, variable_name, 
+              save = False, path = None):
     fig, ax = plt.subplots(figsize=(6, 4,))
 
     if variable_name =='slowness_error':
@@ -105,10 +112,14 @@ def histogram(values, lower_quantile, upper_quantile, variable_name, save = Fals
     
 
 
-    ax.axvspan(xlim1,np.quantile(values, lower_quantile), color = 'gray',alpha = 0.1)
+    ax.axvspan(xlim1,np.quantile(values, lower_quantile), 
+               color = 'gray',alpha = 0.1)
 
-    ax.axvspan(np.quantile(values, upper_quantile),xlim2, color = 'gray',alpha = 0.1)
-    ax.axvspan(np.quantile(values, lower_quantile), np.quantile(values, upper_quantile), color = 'blue',alpha = 0.1)
+    ax.axvspan(np.quantile(values, upper_quantile),xlim2, 
+               color = 'gray',alpha = 0.1)
+    ax.axvspan(np.quantile(values, lower_quantile), 
+               np.quantile(values, upper_quantile), 
+               color = 'blue',alpha = 0.1)
 
     ax.hist(values, **hist_params)
 
@@ -117,8 +128,10 @@ def histogram(values, lower_quantile, upper_quantile, variable_name, save = Fals
     ax.grid(axis='y', alpha=0.8)
 
 
-    ax.axvline(x=np.quantile(values, upper_quantile), ymin = 0, ymax = 1, color = 'black', linestyle = '--')
-    ax.axvline(x=np.quantile(values, lower_quantile), ymin = 0, ymax = 1, color = 'black', linestyle = '--')
+    ax.axvline(x=np.quantile(values, upper_quantile), ymin = 0,
+               ymax = 1, color = 'black', linestyle = '--')
+    ax.axvline(x=np.quantile(values, lower_quantile), ymin = 0, 
+               ymax = 1, color = 'black', linestyle = '--')
     range1 = (np.abs(np.quantile(values, lower_quantile))+np.abs(np.quantile(values, upper_quantile)))
     print('Quantile range:', range1)
 
@@ -128,22 +141,28 @@ def histogram(values, lower_quantile, upper_quantile, variable_name, save = Fals
 
     plt.show()
 
-def baz_error_spatial(baz, baz_error, baz_error_model, color_data, color_data_label, niazi = True, save = False, path = None):
+def baz_error_spatial(baz, baz_error, baz_error_model, color_data, 
+                      color_data_label, niazi = True, save = False, 
+                      path = None):
 
     fig, ax = plt.subplots(figsize = (7,4))
     
     trans = blended_transform_factory(ax.transData, ax.transAxes)
 
     if len(baz_error_model) > 0:
-        ax.scatter(baz, baz_error, color = 'gray', edgecolors = 'black', s = 100, label = 'measured')
-        ax.scatter(baz, baz_error_model, color = 'red', edgecolors = 'black', s = 100, label = 'modeled')
+        ax.scatter(baz, baz_error, color = 'gray', edgecolors = 'black',
+                   s = 100, label = 'measured')
+        ax.scatter(baz, baz_error_model, color = 'red', edgecolors = 'black', 
+                   s = 100, label = 'modeled')
     else:
 
         if len(color_data) > 0:
-            sc = ax.scatter(baz, baz_error, c = color_data, cmap = 'plasma_r', edgecolors = 'black', s = 100)
+            sc = ax.scatter(baz, baz_error, c = color_data, cmap = 'plasma_r',
+                            edgecolors = 'black', s = 100)
             fig.colorbar(sc, label = color_data_label)
         else:
-            ax.scatter(baz, baz_error, color = 'gray', alpha = 1, edgecolors = 'black', s = 100, label = 'observed')
+            ax.scatter(baz, baz_error, color = 'gray', alpha = 1, 
+                       edgecolors = 'black', s = 100, label = 'observed')
 
     
 
@@ -158,12 +177,17 @@ def baz_error_spatial(baz, baz_error, baz_error_model, color_data, color_data_la
         #Plot niazi fit
         Z_fit = np.linspace(0, 360, 500)
         y_fit = cos_model(Z_fit, *params)
-        ax.plot(Z_fit, y_fit, color = 'red', linewidth = 2.5, label = 'Niazi fit', alpha= 0.5)
+        ax.plot(Z_fit, y_fit, color = 'red', linewidth = 2.5, 
+                label = 'Niazi fit', alpha= 0.5)
 
-    ax.text(45,0.9, "NE", transform = trans, color = 'black', fontweight = 'bold',fontsize = 15, ha='center')
-    ax.text(135,0.9, "SE", transform = trans, color = 'black', fontweight = 'bold',fontsize = 15, ha='center')
-    ax.text(225,0.9, "SW", transform = trans, color = 'black', fontweight = 'bold',fontsize = 15, ha='center')
-    ax.text(315,0.9, "NW", transform = trans, color = 'black', fontweight = 'bold',fontsize = 15, ha='center')
+    ax.text(45,0.9, "NE", transform = trans, color = 'black', 
+            fontweight = 'bold',fontsize = 15, ha='center')
+    ax.text(135,0.9, "SE", transform = trans, color = 'black', 
+            fontweight = 'bold',fontsize = 15, ha='center')
+    ax.text(225,0.9, "SW", transform = trans, color = 'black', 
+            fontweight = 'bold',fontsize = 15, ha='center')
+    ax.text(315,0.9, "NW", transform = trans, color = 'black', 
+            fontweight = 'bold',fontsize = 15, ha='center')
 
     ax.axvline(x=90, color = 'black', linestyle = '--')
     ax.axvline(x=180, color = 'black', linestyle = '--')
@@ -186,7 +210,9 @@ def baz_error_spatial(baz, baz_error, baz_error_model, color_data, color_data_la
     plt.show()
 
 
-def slow_error_spatial(baz, slow_error, slow_error_model, color_data, color_data_label, niazi = True, save = False, path = None):
+def slow_error_spatial(baz, slow_error, slow_error_model, color_data,
+                       color_data_label, niazi = True, 
+                       save = False, path = None):
 
     fig, ax = plt.subplots(figsize = (7,4))
     
@@ -194,14 +220,19 @@ def slow_error_spatial(baz, slow_error, slow_error_model, color_data, color_data
 
 
     if len(slow_error_model) > 0:
-        ax.scatter(baz, slow_error, color = 'gray', edgecolors = 'black', s = 100, label = 'measured')
-        ax.scatter(baz, slow_error_model, color = 'skyblue', edgecolors = 'black', s = 100, label = 'modeled')
+        ax.scatter(baz, slow_error, color = 'gray', edgecolors = 'black',
+                   s = 100, label = 'measured')
+        ax.scatter(baz, slow_error_model, color = 'skyblue', 
+                   edgecolors = 'black', s = 100, label = 'modeled')
     else:
         if len(color_data) > 0:
-            sc = ax.scatter(baz, slow_error, c = color_data, cmap = 'cividis_r', edgecolors = 'black', s = 100)
+            sc = ax.scatter(baz, slow_error, c = color_data, 
+                            cmap = 'cividis_r', edgecolors = 'black', s = 100)
             fig.colorbar(sc, label = color_data_label)
         else:
-            ax.scatter(baz, slow_error, color = 'gray', alpha = 1, edgecolors = 'black', s = 100, label = array+' observed')
+            ax.scatter(baz, slow_error, color = 'gray', alpha = 1, 
+                       edgecolors = 'black', s = 100, 
+                       label = array+' observed')
 
     
     if niazi == True:
@@ -215,12 +246,17 @@ def slow_error_spatial(baz, slow_error, slow_error_model, color_data, color_data
         #Plot niazi fit
         Z_fit = np.linspace(0, 360, 500)
         y_fit = cos_model(Z_fit, *params)
-        ax.plot(Z_fit, y_fit, color = 'red', linewidth = 2.5, label = 'Niazi fit', alpha= 0.5)
+        ax.plot(Z_fit, y_fit, color = 'red', linewidth = 2.5, 
+                label = 'Niazi fit', alpha= 0.5)
 
-    ax.text(45,0.9, "NE", transform = trans, color = 'black', fontweight = 'bold',fontsize = 15, ha='center')
-    ax.text(135,0.9, "SE", transform = trans, color = 'black', fontweight = 'bold',fontsize = 15, ha='center')
-    ax.text(225,0.9, "SW", transform = trans, color = 'black', fontweight = 'bold',fontsize = 15, ha='center')
-    ax.text(315,0.9, "NW", transform = trans, color = 'black', fontweight = 'bold',fontsize = 15, ha='center')
+    ax.text(45,0.9, "NE", transform = trans, color = 'black', 
+            fontweight = 'bold',fontsize = 15, ha='center')
+    ax.text(135,0.9, "SE", transform = trans, color = 'black', 
+            fontweight = 'bold',fontsize = 15, ha='center')
+    ax.text(225,0.9, "SW", transform = trans, color = 'black', 
+            fontweight = 'bold',fontsize = 15, ha='center')
+    ax.text(315,0.9, "NW", transform = trans, color = 'black', 
+            fontweight = 'bold',fontsize = 15, ha='center')
 
     ax.axvline(x=90, color = 'black', linestyle = '--')
     ax.axvline(x=180, color = 'black', linestyle = '--')
@@ -250,7 +286,8 @@ def slow_error_spatial(baz, slow_error, slow_error_model, color_data, color_data
 def vida_plot(distmat, slowmat, depmat):
     fig, ax = plt.subplots(figsize=(8,6))
     levels = np.linspace(0, 200, 200)
-    sc = ax.contourf(distmat, I, depmat, levels=levels, cmap='inferno_r', vmin = 0, vmax = 200) #jet_r, 90
+    sc = ax.contourf(distmat, I, depmat, levels=levels, cmap='inferno_r',
+                     vmin = 0, vmax = 200) #jet_r, 90
     ax.set_xlabel('epicentral distance (kilometers)')
     ax.set_ylabel('horizontal slowness (s/km)')
     ax.set_ylim(0,0.20)
@@ -262,7 +299,8 @@ def dist_depth_slow(distmat, slowmat, depmat, slow_cone ):
     fig, ax = plt.subplots(figsize=(8,6))
     level = 0.2/(slow_cone/2)
     levels = np.linspace(0, 0.2, level)
-    sc = ax.contourf(distmat, depmat, I, levels=levels, cmap='inferno', vmin = 0, vmax = 0.2) #jet_r, 90
+    sc = ax.contourf(distmat, depmat, I, levels=levels, cmap='inferno',
+                     vmin = 0, vmax = 0.2) #jet_r, 90
     ax.set_xlabel('epicentral distance (kilometers)')
     ax.set_ylabel('depth (km)')
     ax.set_ylim(0,250)
@@ -278,7 +316,8 @@ def dist_depth_slow(distmat, slowmat, depmat, slow_cone ):
 ############################################################
 
    
-def array_layout(lat_list, lon_list, elev_list, station_names, station_names_sublist = None, save = False, path = None):
+def array_layout(lat_list, lon_list, elev_list, station_names, 
+                 station_names_sublist = None, save = False, path = None):
 
     #Find relative positions in meters of each element to array center
     output = get_geometry(lat_list, lon_list, elev_list, return_center = True)
@@ -297,7 +336,8 @@ def array_layout(lat_list, lon_list, elev_list, station_names, station_names_sub
         
     fig,ax = plt.subplots(figsize = (8,8))
     if station_names_sublist.any() == None:
-        ax.scatter(xpos, ypos, color = 'firebrick',marker = '^', linewidths = 1, s = 300,  edgecolors = 'black')
+        ax.scatter(xpos, ypos, color = 'firebrick',marker = '^', 
+                   linewidths = 1, s = 300,  edgecolors = 'black')
 
     else:
         
@@ -309,8 +349,10 @@ def array_layout(lat_list, lon_list, elev_list, station_names, station_names_sub
             ypos_sub.append(ypos[idx])
             
             
-        ax.scatter(xpos, ypos, color = 'gray',marker = '^', linewidths = 1, s = 300,  edgecolors = 'black')
-        ax.scatter(xpos_sub, ypos_sub, color = 'firebrick',marker = '^', linewidths = 1, s = 300,  edgecolors = 'black')
+        ax.scatter(xpos, ypos, color = 'gray',marker = '^', linewidths = 1, 
+                   s = 300,  edgecolors = 'black')
+        ax.scatter(xpos_sub, ypos_sub, color = 'firebrick',marker = '^', 
+                   linewidths = 1, s = 300,  edgecolors = 'black')
         
     #cornflowerblue
     for i in range(len(xpos)):
@@ -373,7 +415,8 @@ def array_layout1(lat_list, lon_list, elev_list, station_names,
                    linewidths=1, s=300, edgecolors='black')
 
         for i in range(len(station_names_sublist)):
-            ax.text(xpos_sub[i]-100, ypos_sub[i]+80, station_names_sublist[i], weight = 'bold')
+            ax.text(xpos_sub[i]-100, ypos_sub[i]+80, station_names_sublist[i],
+                    weight = 'bold')
 
     ax.set_xlabel("x position (m)")
     ax.set_ylabel("y position (m)")
